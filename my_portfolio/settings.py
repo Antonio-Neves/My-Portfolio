238ef567ca280dc2b1aa8d137bec199924b548ea
antonio <it-an@hotmail.com>
2021-08-06 18:22:52 -0300

    Configured email settings

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -226,17 +226,15 @@ if DEBUG:
 
 # --- Production --- #
 if not DEBUG:
-	# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 	EMAIL_HOST = config('EMAIL_HOST')
 	EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 	EMAIL_PORT = config('EMAIL_PORT', cast=int)
-	# EMAIL_USE_TLS = True
-	EMAIL_USE_SSL = True
+	EMAIL_USER_SSL = True
 	EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 	DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
-	ADMINS = [(config('SUPER_USER'), config('EMAIL'))]
-	MANAGERS = ADMINS
+	# ADMINS = [(config('SUPER_USER'), config('EMAIL'))]
+	# MANAGERS = ADMINS
 
 
 # ----------------------------------------------------------
