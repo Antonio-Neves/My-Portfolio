52c44e471f9b13592888639d291e7348f1d5ccd9
antonio <it-an@hotmail.com>
2021-08-06 16:45:49 -0300

    Included paths for Apps

--- my_portfolio/urls.py
+++ my_portfolio/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from decouple import config
+
+ADMIN_SITE = config('ADMIN_SITE')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+	path('accounts/', include('accounts.urls')),
+	path('', include('principal.urls')),
+	path(ADMIN_SITE, admin.site.urls),
 ]
