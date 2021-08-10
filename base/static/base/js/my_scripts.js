7cf0699b7ffa3d84c724305aa94de44449bece03
antonio <it-an@hotmail.com>
2021-08-10 20:43:42 -0300

    Cleaned JavaScript code

--- base/static/base/js/my_scripts.js
+++ base/static/base/js/my_scripts.js
@@ -32,33 +32,6 @@ navbarLinksContainer.addEventListener("click", (clickEvent) => {
 navbarMenu.addEventListener("click", closeMobileNavbar);
 
 //
-// --- Animated Title ---
-//
-// Wrap every letter in a span
-// var textWrapper = document.querySelector('.ml12');
-// textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-//
-// anime.timeline({loop: true})
-//   // Entry
-//   .add({
-//     targets: '.ml12 .letter',
-//     translateX: [40,0],
-//     translateZ: 0,
-//     opacity: [0,1],
-//     easing: "easeOutExpo",
-//     duration: 1200,
-//     delay: (el, i) => 500 + 30 * i
-//   // Exit
-//   }).add({
-//     targets: '.ml12 .letter',
-//     translateX: [0, 30],
-//     opacity: [1,0],
-//     easing: "easeInExpo",
-//     duration: 1100,
-//     delay: (el, i) => 100 + 30 * i
-//   });
-
-//
 // --- Cokie Consent ---
 //
 window.cookieconsent.initialise({
