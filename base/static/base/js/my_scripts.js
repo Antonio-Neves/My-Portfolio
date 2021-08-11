553c2a1c78b228cd8d6796f75c138f75a4982bb9
antonio <it-an@hotmail.com>
2021-08-11 10:32:09 -0300

    Turned off site preload

--- base/static/base/js/my_scripts.js
+++ base/static/base/js/my_scripts.js
@@ -1,27 +1,27 @@
-/* Preloader
-    * -------------------------------------------------- */
-    var ssPreloader = function() {
-
-        $("html").addClass('ss-preload');
-
-        $WIN.on('load', function() {
-
-            //force page scroll position to top at page refresh
-            $('html, body').animate({ scrollTop: 0 }, 'normal');
-
-            // will first fade out the loading animation
-            $("#loader").fadeOut("slow", function() {
-                // will fade out the whole DIV that covers the website.
-                $("#preloader").delay(300).fadeOut("slow");
-            });
-
-            // for hero content animations
-            $("html").removeClass('ss-preload');
-            $("html").addClass('ss-loaded');
-
-        });
-    };
-
+// /* Preloader
+//     * -------------------------------------------------- */
+//     var ssPreloader = function() {
+//
+//         $("html").addClass('ss-preload');
+//
+//         $WIN.on('load', function() {
+//
+//             //force page scroll position to top at page refresh
+//             $('html, body').animate({ scrollTop: 0 }, 'normal');
+//
+//             // will first fade out the loading animation
+//             $("#loader").fadeOut("slow", function() {
+//                 // will fade out the whole DIV that covers the website.
+//                 $("#preloader").delay(300).fadeOut("slow");
+//             });
+//
+//             // for hero content animations
+//             $("html").removeClass('ss-preload');
+//             $("html").addClass('ss-loaded');
+//
+//         });
+//     };
+//
 //
 // --- Navbar ---
 //
