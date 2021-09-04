339f94b220bdb971846f87677bbc1824ce46d1ed
antonio <it-an@hotmail.com>
2021-09-04 18:07:36 -0300

    Created form and view for contact form

---
+++ principal/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from django.core.mail.message import EmailMessage
+from decouple import config
+
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+
+
+class ContactForm(forms.Form):
+	contact_name = forms.CharField(max_length=100)
+	contact_email = forms.CharField(max_length=100)
+	contact_message = forms.CharField(widget=forms.Textarea())
+
+	def send_mail(self):
+
+		contact_name = self.cleaned_data['contact_name']
+		contact_email = self.cleaned_data['contact_email']
+		contact_message = self.cleaned_data['contact_message']
+
+		contact_content = 'Name: {}\nEmail: {}\nMessage: {}'\
+			.format(contact_name, contact_email, contact_message)
+
+		mail = EmailMessage(
+			subject='Email from My Portfolio',
+			body=contact_content,
+			from_email=EMAIL_HOST_USER,
+			to=[EMAIL_HOST_USER,],
+			headers={'Reply-to': contact_email}
+		)
+		mail.send()
--- principal/views.py
+++ principal/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 from django.http import FileResponse
 from my_portfolio.settings import BASE_DIR
-from django.views.generic import TemplateView
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+
+from principal.forms import ContactForm
 
 
 # --- Home Page --- #
-class IndexView(TemplateView):
+class IndexView(FormView):
 	template_name = 'principal/index.html'
+	form_class = ContactForm
+	success_url = reverse_lazy('index')
+
+	def form_valid(self, form, *args, **kwargs):
+		form.send_mail()
+		return super(IndexView, self).form_valid(form, *args, **kwargs)
+
+	def form_invalid(self, form, *args, **kwargs):
+		return super(IndexView, self).form_invalid(form, *args, **kwargs)
 
 
+# --- Download CV --- #
 def download_cv(request):
 	file_path = BASE_DIR / 'files/Cv_Antonio_Neves_2021.pdf'
 
