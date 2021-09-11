53e16f257602f817f1c43a22ee4caa0b87bceed2
antonio <it-an@hotmail.com>
2021-09-11 11:23:59 -0300

    Created view for Article

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView, DetailView
 
-# Create your views here.
+from blog.models import Category, SubCategory, Article
+
+
+# --- Articles --- #
+class ArticleDetailView(DetailView):
+	model = Article
+	slug_field = 'article_slug'
+	template_name = 'blog/article-detail.html'
+	# context_object_name = 'article'
