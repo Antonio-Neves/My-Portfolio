7007d0279b4180f8ae2cc00bcdeff519aff664b2
antonio <it-an@hotmail.com>
2021-10-02 13:40:21 -0300

    Added functions to contact form
    Added FormView class and form functions to the view

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import FormView
 from django.views.generic import TemplateView, DetailView
 
+from principal.forms import ContactForm
 from blog.models import Category, SubCategory, Article
 
 
 # --- Articles --- #
-class ArticleDetailView(DetailView):
+class ArticleDetailView(DetailView, FormView):
 	model = Article
 	slug_field = 'article_slug'
 	template_name = 'blog/article-detail.html'
-	# context_object_name = 'article'
+	form_class = ContactForm
+	success_url = reverse_lazy('index')
+
+	def form_valid(self, form, *args, **kwargs):
+		form.send_mail()
+		return super(ArticleDetailView, self).form_valid(form)
+
+	def form_invalid(self, form, *args, **kwargs):
+		return super(ArticleDetailView, self).form_invalid(form)
