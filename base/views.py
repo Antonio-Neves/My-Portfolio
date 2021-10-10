92843116865227359e314ac628c09bae6a8be1f2
antonio <it-an@hotmail.com>
2021-10-10 15:13:52 -0300

    Created base view for email form in footer

--- base/views.py
+++ base/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import FormView
+from principal.forms import ContactForm
 
-# Create your views here.
+
+class ContactFormView(FormView):
+	form_class = ContactForm
+	success_url = reverse_lazy('index')
+
+	def form_valid(self, form, *args, **kwargs):
+		form.send_mail()
+		return super().form_valid(form)
+
+	def form_invalid(self, form, *args, **kwargs):
+		return super().form_invalid(form)
