9c403d7e893c074b059292f1c06db298a3e428c0
antonio <it-an@hotmail.com>
2021-10-11 14:27:45 -0300

    Changed Get method because if article is not published return page not found

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from django.views.generic import DetailView
 from base.views import ContactFormView
 
@@ -9,3 +11,17 @@ class ArticleDetailView(DetailView, ContactFormView):
 	model = Article
 	slug_field = 'article_slug'
 	template_name = 'blog/article-detail.html'
+
+	def get(self, request, *args, **kwargs):
+		"""
+		Changed Get method because if article is not published
+		return page not found
+		"""
+
+		self.object = self.get_object()
+		context = self.get_context_data(object=self.object)
+
+		if self.object.article_status == '1':
+			return self.render_to_response(context)
+		else:
+			raise Http404
