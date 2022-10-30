4c366578d25f1c07d4963c0050d40b7a1ae20f63
antonio <antonio.nevesfyld@hotmail.com>
2022-10-30 19:25:22 +0000

    Changed cookies

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -43,6 +43,7 @@ if not DEBUG:
     # SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    CSRF_TRUSTED_ORIGINS = [config('ALLOWED_HOSTS')]
 
 # ----------------------------------------------------------
 # Application definition
