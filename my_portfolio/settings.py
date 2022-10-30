62bb0ada97304f48988ad0e5eefe50b2532d7428
antonio <antonio.nevesfyld@hotmail.com>
2022-10-30 16:16:55 +0000

    Changed to original settings file

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -24,8 +24,8 @@ SECRET_KEY = config('SECRET_KEY')
 
 # ----------------------------------------------------------
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = False
+DEBUG = config('DEBUG', default=False, cast=bool)
+
 
 # ----------------------------------------------------------
 # Allowed Hosts
