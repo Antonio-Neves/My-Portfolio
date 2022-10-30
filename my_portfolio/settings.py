8b3111ed76a4f729cb58e82d4c5eac99a66aea08
antonio <antonio.nevesfyld@hotmail.com>
2022-10-30 17:29:17 +0000

    Changed to original settings file

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -237,12 +237,12 @@ if DEBUG:
 
 # --- Production --- #
 if not DEBUG:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = config('EMAIL_HOST')
     EMAIL_HOST_USER = config('EMAIL_HOST_USER')
     EMAIL_PORT = config('EMAIL_PORT', cast=int)
-    EMAIL_USE_SSL = True
-    # EMAIL_USE_TLS = True
+    # EMAIL_USE_SSL = True
+    EMAIL_USE_TLS = True
     EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
